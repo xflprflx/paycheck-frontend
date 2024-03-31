619c43878c13e1ed65981dd3084a85966d72133c
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-03-31 19:23:38 -0300

    nav created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+// Componentes do projeto
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
