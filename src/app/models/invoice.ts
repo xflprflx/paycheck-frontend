6720043a3e1676f618356703d676860525441232
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-04-02 11:16:10 -0300

    ok

--- src/app/models/invoice.ts
+++ src/app/models/invoice.ts
@@ -1,8 +1,12 @@
-export interface Invoice {
-    id?: string;
-    number: string;
-    deliveryStatus: string;
-    scannedDate: Date;
-    createdAt: Date;
-    updatedAt: Date;
+export class Invoice {
+  id?: string;
+  number: string;
+  deliveryStatus: string;
+  scannedDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(number: string) {
+    this.number = number;
+  }
 }
