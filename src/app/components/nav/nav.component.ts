92f83f5c572af732b1e91474e7a5a596c440895a
Filipe Machado <x.filipe.machado.x@gmail.com>
2024-04-29 15:55:45 -0300

    tooltip

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,14 +1,14 @@
-import { UploadEventsService } from "src/app/services/upload-events.service";
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from "@angular/core";
-import { Router } from "@angular/router";
-import { TransportDocumentService } from "src/app/services/transport-document.service";
-import { PaymentService } from "src/app/services/payment.service";
-import { ModelEventService } from "src/app/services/model-event.service";
-import { Payment } from "src/app/models/payment";
-import { TransportDocument } from "src/app/models/transport-document";
-import { DashboardEventService } from "src/app/services/dashboard-event.service";
-import { MatDrawer } from "@angular/material/sidenav";
-import { NavStateService } from "src/app/services/nav-state.service";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { Payment } from 'src/app/models/payment';
+import { TransportDocument } from 'src/app/models/transport-document';
+import { DashboardEventService } from 'src/app/services/dashboard-event.service';
+import { ModelEventService } from 'src/app/services/model-event.service';
+import { NavStateService } from 'src/app/services/nav-state.service';
+import { PaymentService } from 'src/app/services/payment.service';
+import { TransportDocumentService } from 'src/app/services/transport-document.service';
+import { UploadEventsService } from 'src/app/services/upload-events.service';
 
 @Component({
   selector: "app-nav",
@@ -21,6 +21,7 @@ export class NavComponent implements OnInit {
   showTableValue: boolean = false;
   transportDocuments: TransportDocument[];
   payments: Payment[];
+  tooltip: string = this.showTableValue ? 'Recolher tabela' : 'Mostrar tabela'
   @ViewChild("drawer") drawer: MatDrawer;
 
   constructor(
